4c8a722ac17487bc7c31b000cd3cf8c922e5cd21
Akash <hafejmohammadakash@gmail.com>
2025-05-17 12:12:44 +0600

    from setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-import './App.css'
+import "./App.css";
 
 function App() {
 
 
+  const handleFormSubmit = (event) => {
+    event.preventDefault()
+
+    const form = event.target;
+
+    const name = form.name.value;
+    const email = form.email.value;
+    
+    const users = {name, email}
+     console.log(users)
+
+     fetch
+
+
+
+  }
+
+
+
+
   return (
     <>
-
-      <h1>Vite + React</h1>
-
+      <h1>Simple CRUD</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input type="text" name="name" />
+        <br />
+        <input type="email" name="email" />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
