fefa5f9e2c02d81e122b9606cdad4bd87a20bc88
Akash <hafejmohammadakash@gmail.com>
2025-05-17 14:41:11 +0600

    post method setup

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,25 @@ function App() {
 
     const name = form.name.value;
     const email = form.email.value;
-    
-    const users = {name, email}
-     console.log(users)
 
-     fetch
+    const user = {name, email}
+     console.log(user)
 
+     fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type" : "application/json"
+      },
+      body: JSON.stringify(user)
+     })
+     .then(res => res.json())
+     .then(data => {
+      // users 
+      console.log(data)
+
+      // reset form
+      form.reset()
+     })
 
 
   }
