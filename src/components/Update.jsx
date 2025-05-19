b12be0771e7618dc76f2c2b203a385ab522ed167
Akash <hafejmohammadakash@gmail.com>
2025-05-19 12:30:35 +0600

    ubdate users feth ore data anlam

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -3,18 +3,30 @@ import { useLoaderData } from "react-router-dom";
 
 const Update = () => {
   const lodedUsers = useLoaderData();
-  const { name, email } = lodedUsers || {};
+  const { _id, name, email } = lodedUsers || {};
 
   const handleUpdateUser = (e) => {
     e.preventDefault();
- 
+
     const form = e.target;
 
     const name = form.name.value;
     const email = form.email.value;
+    const updatedUser = {name, email}
 
-    console.log(name, email)
-    
+    console.log(updatedUser);
+
+    fetch(`http://localhost:5000/users/${_id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(updatedUser)
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
   };
 
   return (
